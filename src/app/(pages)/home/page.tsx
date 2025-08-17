deb87d8e406684fc1c77a9d6a543db98940b41ad
Muhammad Talha <talha18513@gmail.com>
2025-08-16 20:54:14 -0700

    hero section created

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 const HomePage = () => {
     return (
         <WrapperLayout className='relative '>
-            <iframe src='https://my.spline.design/thresholddarkambientuicopy-TdFiRssh4jmmRXTHTQPp1U0N/' className="w-full absolute top-0 left-0 h-screen bg-black -z-10"></iframe>
+            <iframe src='https://my.spline.design/thresholddarkambientuicopy-TdFiRssh4jmmRXTHTQPp1U0N/' className="w-full absolute top-0 left-0 h-dvh bg-black -z-10"></iframe>
             <div className="absolute md:bottom-8 bottom-2 left-5">
                 <CircularText
                     text="FULL STACK DEVELOPMENT "
@@ -17,7 +17,7 @@ const HomePage = () => {
                     className="font-normal"
                 />
             </div>
-            <div className="min-h-screen  w-full flex items-center justify-center flex-col">
+            <div className="h-dvh  w-full flex items-center justify-center flex-col">
                 <div className='absolute bottom-1 cursor-default right-3 h-20'>
                     <GlareHover
                         glareColor="#ffffff"
